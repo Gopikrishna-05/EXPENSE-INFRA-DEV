72b4ff602f826d5b4ad8512ff55853da58433b65
Gopikrishna-05 <gopikrishna.g.workmail@gmail.com>
2026-06-22 17:04:27 +0530

    allow bastion access to app ALB via security group rule

--- sg-10/sg.tf
+++ sg-10/sg.tf
@@ -47,3 +47,13 @@ module "app_alb_sg" {
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     common_tags = var.common_tags
 }
+
+# APP ALB accepting traffic from bastion
+resource "aws_security_group_rule" "app_alb_bastion" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  source_security_group_id       = module.bastion_sg.sg_id
+  security_group_id = module.app_alb_sg.sg_id
+}
