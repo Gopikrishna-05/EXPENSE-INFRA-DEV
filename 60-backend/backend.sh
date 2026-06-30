67af7297cec165a252c0f867834c0b593e0b34c8
Gopikrishna-05 <gopikrishna.g.workmail@gmail.com>
2026-06-30 13:22:03 +0530

    fix: switch ansible-pull playbook from backend.yaml to main.yaml

--- 60-backend/backend.sh
+++ 60-backend/backend.sh
@@ -4,5 +4,5 @@ dnf install ansible -y
 # push
 # ansible-playbook -i inventory mysql.yaml
 
-ansible-pull  -i localhost, -U https://github.com/Gopikrishna-05/EXPENSE-ANSIBLE-ROLES-TF.git backend.yaml -e COMPONENT=backend -e ENVIRONMENT=$1
+ansible-pull  -i localhost, -U https://github.com/Gopikrishna-05/EXPENSE-ANSIBLE-ROLES-TF.git main.yaml -e COMPONENT=backend -e ENVIRONMENT=$1
 
