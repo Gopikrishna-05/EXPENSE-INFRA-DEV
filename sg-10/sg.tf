838c5a7ff958f6d74f876988a4026b04fadb132b
Gopikrishna-05 <gopikrishna.g.workmail@gmail.com>
2026-07-01 17:54:21 +0530

    feat: add web ALB SG with HTTPS rule and store SG ID in SSM

--- sg-10/sg.tf
+++ sg-10/sg.tf
@@ -58,6 +58,16 @@ module "app_alb_sg" {
     common_tags = var.common_tags
 }
 
+module "web_alb_sg" {
+    source = "git::https://github.com/Gopikrishna-05/TERRAFORM-AWS-VPC-MODULE.git//terraform-aws-securitygroup?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "web-alb"
+    sg_description = "Created for frontend ALB in expense dev"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
+
 # APP ALB accepting traffic from bastion
 resource "aws_security_group_rule" "app_alb_bastion" {
   type              = "ingress"
@@ -176,4 +186,13 @@ resource "aws_security_group_rule" "backend_app_alb" {
   protocol          = "tcp"
   source_security_group_id = module.app_alb_sg.sg_id
   security_group_id = module.backend_sg.sg_id
+}
+
+resource "aws_security_group_rule" "web_alb_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.web_alb_sg.sg_id
 }
